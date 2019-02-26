8a5691afdb7a3fc4ebb9fb69530aca1648409b13
Hatem Jaber <hatem@7079.net>
2019-02-25 19:20:37 -0500

    changed package.json

--- gh-pages.js
+++ gh-pages.js
@@ -1,6 +1,6 @@
 const ghpages = require('gh-pages');
 
 ghpages.publish('moneyquest', {
-    branch: 'test',
+    branch: 'moneyquest',
     repo: 'git@github.com:inonso/sites.git'
 }, err => { console.log(err) })
